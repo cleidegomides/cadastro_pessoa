ffcafa5fbc5f13f79a12e4fda995868c6c3b68a7
Cleideg Gomides <cleidegomides@hotmail.com>
2020-10-19 11:07:00 -0300

    Alteração Readme

--- src/main/java/com/everis/cadastro/model/dto/endereco/EnderecoRequestDTO.java
+++ src/main/java/com/everis/cadastro/model/dto/endereco/EnderecoRequestDTO.java
@@ -1,6 +1,5 @@
 package com.everis.cadastro.model.dto.endereco;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/everis/cadastro/model/entities/Pessoa.java
+++ src/main/java/com/everis/cadastro/model/entities/Pessoa.java
@@ -1,7 +1,6 @@
 package com.everis.cadastro.model.entities;
 
 import lombok.*;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
